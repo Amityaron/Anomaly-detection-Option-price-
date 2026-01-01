191314182499d1a24c7f7840e9017e10ebfb2ad4
Amit Yaron <70226919+Amityaron@users.noreply.github.com>
2026-01-01 20:29:48 +0200

    Update Z_score_app.py

--- pages/Z_score_app.py
+++ pages/Z_score_app.py
@@ -66,8 +66,15 @@ for etf in etfs:
     mean_last_month = float(data.mean())
     std_last_month = float(data.std())
 
-    skewness_last_month = round(float(skew(data)), 2) if len(data) > 1 else 0
-    kurtosis_last_month = round(float(kurtosis(data)), 2) if len(data) > 1 else 0
+    #skewness_last_month = round(float(skew(data)), 2) if len(data) > 1 else 0
+    #kurtosis_last_month = round(float(kurtosis(data)), 2) if len(data) > 1 else 0
+    values = data.astype(float).values  # force numeric 1D array
+    if len(values) > 1:
+        skewness_last_month = round(skew(values, nan_policy='omit'), 2)
+        kurtosis_last_month = round(kurtosis(values, nan_policy='omit'), 2)
+    else:
+        skewness_last_month = 0
+        kurtosis_last_month = 0
     current_price = float(data.iloc[-1])
 
     z_score_current_price = round((current_price - mean_last_month) / std_last_month, 2) if std_last_month != 0 else 0
@@ -86,4 +93,5 @@ df = pd.DataFrame(results)
 # Fill NaN values and sort by Z-Score
 df_sorted = df.sort_values(by='Z Score', ascending=True)
 st.table(df_sorted)
+
 
